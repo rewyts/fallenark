b350051546420d55cdeae4df950dd375a668c064
Neo <neo@lihengji.com>
2017-05-05 18:34:34 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+##FallenArk的Disucz主题
---
+++ assets/build/config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const { argv } = require('yargs');
+const merge = require('webpack-merge');
+
+const userConfig = require('../config');
+
+const isProduction = !!((argv.env && argv.env.production) || argv.p);
+const rootPath = (userConfig.paths && userConfig.paths.root)
+  ? userConfig.paths.root
+  : process.cwd();
+
+const config = merge({
+  copy: 'images/**/*',
+  proxyUrl: 'http://localhost:3000',
+  cacheBusting: '[name]_[hash]',
+  paths: {
+    root: rootPath,
+    assets: path.join(rootPath, 'assets'),
+    dist: path.join(rootPath, 'dist'),
+  },
+  enabled: {
+    sourceMaps: !isProduction,
+    optimize: isProduction,
+    cacheBusting: isProduction,
+    watcher: !!argv.watch,
+  },
+  watch: [],
+  browsers: [],
+}, userConfig);
+
+module.exports = merge(config, {
+  env: Object.assign({ production: isProduction, development: !isProduction }, argv.env),
+  publicPath: `${config.publicPath}/${path.basename(config.paths.dist)}/`,
+  manifest: {},
+});
+
+if (process.env.SAGE_DIST_PATH) {
+  module.exports.publicPath = process.env.SAGE_DIST_PATH;
+}
